ab8109a4b38d2a70b9b48e48d9258556e0acb149
yash <yashtilala412@gmail.com>
2024-07-10 18:36:59 +0530

    full screen min height

--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -32,7 +32,7 @@ const Notifications = () => {
         </Badge>
       </div>
       {callAccepted && (
-    <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '10px', order: 1 }}>
+    <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '10px', minHeight: '50px' }}>
 
 
 
