8f89b11a74356cd936801c011877fdeb7905df0e
yash <yashtilala412@gmail.com>
2024-07-10 18:38:37 +0530

    full screen background gradient

--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -32,7 +32,7 @@ const Notifications = () => {
         </Badge>
       </div>
       {callAccepted && (
-    <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '10px', minWidth: '100px' }}>
+    <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '10px', background: 'linear-gradient(to right, #ff7e5f, #feb47b)' }}>
 
 
 
