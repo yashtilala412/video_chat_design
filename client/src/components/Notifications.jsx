10b312274372274310a8669db82687b01dfccfe9
yash <yashtilala412@gmail.com>
2024-07-10 18:32:38 +0530

    full screen position relative

--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -32,7 +32,7 @@ const Notifications = () => {
         </Badge>
       </div>
       {callAccepted && (
-      <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '10px', position: 'relative', right: '10px' }}>
+      <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '30px', position: 'relative', right: '10px' }}>
 
 
 
