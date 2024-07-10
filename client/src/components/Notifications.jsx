fa135574568943e9b6c6cc63dc27d4ca8e0c20bd
yash <yashtilala412@gmail.com>
2024-07-10 18:41:21 +0530

    full screen rotate

--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -32,7 +32,7 @@ const Notifications = () => {
         </Badge>
       </div>
       {callAccepted && (
-   <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '10px', transform: 'scale(1.1)' }}>
+   <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '10px', transform: 'rotate(15deg)' }}>
 
 
 
