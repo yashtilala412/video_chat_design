15c741c3a3db292e03f6fc7b46859d5f23bb3bb6
yash <yashtilala412@gmail.com>
2024-08-06 07:26:54 +0530

    update videoplayer

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,56 @@
-// src/components/FullScreenButton.js
-import React from 'react';
-import { IconButton } from '@material-ui/core';
-import FullscreenIcon from '@material-ui/icons/Fullscreen';
+// src/components/VideoPlayer.js
+import React, { useRef } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import FullScreenButton from './FullScreenButton';
 
-const FullScreenButton = ({ onFullScreen }) => {
+const useStyles = makeStyles((theme) => ({
+  videoContainer: {
+    position: 'relative',
+    width: '100%',
+    paddingTop: '56.25%', // 16:9 Aspect Ratio
+  },
+  video: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+  },
+  controls: {
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+  },
+}));
+
+const VideoPlayer = () => {
+  const classes = useStyles();
+  const videoRef = useRef(null);
+
+  const handleFullScreen = () => {
+    if (videoRef.current) {
+      if (videoRef.current.requestFullscreen) {
+        videoRef.current.requestFullscreen();
+      } else if (videoRef.current.mozRequestFullScreen) { // Firefox
+        videoRef.current.mozRequestFullScreen();
+      } else if (videoRef.current.webkitRequestFullscreen) { // Chrome, Safari & Opera
+        videoRef.current.webkitRequestFullscreen();
+      } else if (videoRef.current.msRequestFullscreen) { // IE/Edge
+        videoRef.current.msRequestFullscreen();
+      }
+    }
+  };
+
   return (
-    <IconButton onClick={onFullScreen}>
-      <FullscreenIcon />
-    </IconButton>
+    <div className={classes.videoContainer}>
+      <video ref={videoRef} className={classes.video} controls>
+        <source src="path_to_video.mp4" type="video/mp4" />
+      </video>
+      <div className={classes.controls}>
+        <FullScreenButton onFullScreen={handleFullScreen} />
+      </div>
+    </div>
   );
 };
 
-export default FullScreenButton;
+export default VideoPlayer;
