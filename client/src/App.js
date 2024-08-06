3af26397cac23b85aae3831423f2a4ed2e1136e9
yash <yashtilala412@gmail.com>
2024-08-06 07:28:09 +0530

    update volume control

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,68 @@
-// src/components/VolumeControl.js
-import React from 'react';
-import { IconButton, Slider } from '@material-ui/core';
-import VolumeUp from '@material-ui/icons/VolumeUp';
+// src/components/VideoPlayer.js
+import React, { useRef, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import FullScreenButton from './FullScreenButton';
+import VolumeControl from './VolumeControl';
 
-const VolumeControl = ({ volume, onVolumeChange }) => {
+const useStyles = makeStyles((theme) => ({
+  videoContainer: {
+    position: 'relative',
+    width: '100%',
+    paddingTop: '56.25%', // 16:9 Aspect Ratio
+  },
+  video: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+  },
+  controls: {
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+    display: 'flex',
+    alignItems: 'center',
+  },
+}));
+
+const VideoPlayer = () => {
+  const classes = useStyles();
+  const videoRef = useRef(null);
+  const [volume, setVolume] = useState(100);
+
+  const handleFullScreen = () => {
+    if (videoRef.current) {
+      if (videoRef.current.requestFullscreen) {
+        videoRef.current.requestFullscreen();
+      } else if (videoRef.current.mozRequestFullScreen) { // Firefox
+        videoRef.current.mozRequestFullScreen();
+      } else if (videoRef.current.webkitRequestFullscreen) { // Chrome, Safari & Opera
+        videoRef.current.webkitRequestFullscreen();
+      } else if (videoRef.current.msRequestFullscreen) { // IE/Edge
+        videoRef.current.msRequestFullscreen();
+      }
+    }
+  };
+
+  const handleVolumeChange = (newValue) => {
+    setVolume(newValue);
+    if (videoRef.current) {
+      videoRef.current.volume = newValue / 100;
+    }
+  };
+
   return (
-    <div>
-      <IconButton>
-        <VolumeUp />
-      </IconButton>
-      <Slider
-        value={volume}
-        onChange={(e, newValue) => onVolumeChange(newValue)}
-        aria-labelledby="volume-slider"
-      />
+    <div className={classes.videoContainer}>
+      <video ref={videoRef} className={classes.video} controls>
+        <source src="path_to_video.mp4" type="video/mp4" />
+      </video>
+      <div className={classes.controls}>
+        <FullScreenButton onFullScreen={handleFullScreen} />
+        <VolumeControl volume={volume} onVolumeChange={handleVolumeChange} />
+      </div>
     </div>
   );
 };
 
-export default VolumeControl;
+export default VideoPlayer;
