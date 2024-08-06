42c14e98cc9c7fdb9a603fcbf7d96b2263c78dac
yash <yashtilala412@gmail.com>
2024-08-06 07:26:07 +0530

    fullscreen button

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,14 @@
+// src/components/FullScreenButton.js
 import React from 'react';
-import { Typography, AppBar, Grid, Container } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { IconButton } from '@material-ui/core';
+import FullscreenIcon from '@material-ui/icons/Fullscreen';
 
-import VideoPlayer from './components/VideoPlayer';
-import Sidebar from './components/Sidebar';
-import Notifications from './components/Notifications';
-
-const useStyles = makeStyles((theme) => ({
-  appBar: {
-    borderRadius: 15,
-    margin: '30px 100px',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '600px',
-    border: '2px solid black',
-
-    [theme.breakpoints.down('xs')]: {
-      width: '90%',
-      margin: '30px 0',
-    },
-  },
-  image: {
-    marginLeft: '15px',
-  },
-  wrapper: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    width: '100%',
-  },
-}));
-
-const App = () => {
-  const classes = useStyles();
-
+const FullScreenButton = ({ onFullScreen }) => {
   return (
-    <Container maxWidth="lg" className={classes.wrapper}>
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography variant="h2" align="center">Video Chat</Typography>
-      </AppBar>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={8}>
-          <VideoPlayer />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Sidebar>
-            <Notifications />
-          </Sidebar>
-        </Grid>
-      </Grid>
-    </Container>
+    <IconButton onClick={onFullScreen}>
+      <FullscreenIcon />
+    </IconButton>
   );
 };
 
-export default App;
+export default FullScreenButton;
